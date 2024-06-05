a13aa8a0882b9beba3540e3108a4b194cc42c390
Adityan Chanduruthil <91912827+spl3ndid@users.noreply.github.com>
2024-06-05 23:17:51 +0000

    Add initial files for To-Do List application
    - Added index.html: The main HTML structure of the To-Do List app
    - Added script.js: Contains JavaScript functions for task management
    - Added style.css: Styles for the To-Do List app

---
+++ app/script.js
@@ -0,0 +1,28 @@
+const inputbox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addtask(){
+    if(inputbox.value==""){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+}
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName == "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName == "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+
